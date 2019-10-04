0e3886accc5ad348a17e3247eff4abe198ae6542
Toke Høiland-Jørgensen <toke@redhat.com>
2019-10-04 18:44:08 +0200

    libbpf: Add syscall wrappers for BPF_PROG_CHAIN_* commands
    This adds simple syscall wrappers for the new BPF_PROG_CHAIN_* commands to
    libbpf.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- tools/lib/bpf/bpf.h
+++ tools/lib/bpf/bpf.h
@@ -171,6 +171,10 @@ LIBBPF_API int bpf_task_fd_query(int pid, int fd, __u32 flags, char *buf,
 				 __u32 *buf_len, __u32 *prog_id, __u32 *fd_type,
 				 __u64 *probe_offset, __u64 *probe_addr);
 
+LIBBPF_API int bpf_prog_chain_add(int prev_prog_fd, __u32 retcode, int next_prog_fd);
+LIBBPF_API int bpf_prog_chain_del(int prev_prog_fd, __u32 retcode);
+LIBBPF_API int bpf_prog_chain_get(int prev_prog_fd, __u32 retcode, __u32 *prog_id);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
