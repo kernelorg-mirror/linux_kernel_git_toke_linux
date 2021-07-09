25ef70f292d81938e4f6c56154ba8d4200931a64
Toke Høiland-Jørgensen <toke@redhat.com>
2021-07-09 17:51:13 +0200

    xdp: Add dequeue program type for getting packets from a PIFO
    Add a new BPF_PROG_TYPE_XDP_DEQUEUE, which will be executed by a new device
    hook to retrieve queued packets for transmission. The API of the dequeue
    program is simple: it takes a context object containing as its sole member
    the ifindex of the device it is being executed on. The program can return a
    pointer to a packet, or NULL to indicate it has nothing to transmit at this
    time. Packet pointers are obtained by dequeueing them from a PIFO
    map (using a helper added in a subsequent commit).
    
    This commit adds dequeue program type and the ability to run it using the
    bpf_prog_run() syscall (returning the dequeued packet to userspace); a
    subsequent commit introduces the network stack hook to attach and execute
    dequeue programs.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -973,6 +973,7 @@ enum bpf_prog_type {
 	BPF_PROG_TYPE_LSM,
 	BPF_PROG_TYPE_SK_LOOKUP,
 	BPF_PROG_TYPE_SYSCALL, /* a program that can execute syscalls */
+	BPF_PROG_TYPE_XDP_DEQUEUE,
 };
 
 enum bpf_attach_type {
@@ -6024,6 +6025,10 @@ struct xdp_md {
 	__u32 rx_queue_index;  /* rxq->queue_index  */
 
 	__u32 egress_ifindex;  /* txq->dev->ifindex */
+};
+
+struct dequeue_ctx {
+	__u32 egress_ifindex;
 };
 
 /* DEVMAP map-value layout
