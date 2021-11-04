9efe309c6d21ed19f06e0f9c9630c8dc10d2dd4d
Toke Høiland-Jørgensen <toke@redhat.com>
2021-11-04 21:32:48 +0100

    bpf: Add helpers to dequeue from a PIFO map
    This adds a new helper to dequeue a packet from a PIFO map,
    bpf_packet_dequeue(). The helper returns a refcounted pointer to the packet
    dequeued from the map; the reference must be released either by dropping
    the packet (using bpf_packet_drop()), or by returning it to the caller.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- tools/include/uapi/linux/bpf.h
+++ tools/include/uapi/linux/bpf.h
@@ -5186,6 +5186,23 @@ union bpf_attr {
  * 	Return
  * 		Map value associated to *key* on *cpu*, or **NULL** if no entry
  * 		was found or *cpu* is invalid.
+ *
+ * long bpf_packet_dequeue(void *ctx, struct bpf_map *map, u64 flags, u64 *rank)
+ *	Description
+ *		Dequeue the packet at the head of the PIFO in *map* and return a pointer
+ *		to the packet (or NULL if the PIFO is empty).
+ *	Return
+ *		On success, a pointer to the packet, or NULL if the PIFO is empty. The
+ *		packet pointer must be freed using *bpf_packet_drop()* or returning
+ *		the packet pointer. The *rank* pointer will be set to the rank of
+ *		the dequeued packet on success, or a negative error code on error.
+ *
+ * long bpf_packet_drop(void *ctx, void *pkt)
+ *	Description
+ *		Drop *pkt*, which must be a reference previously returned by
+ *		*bpf_packet_dequeue()* (and checked to not be NULL).
+ *	Return
+ *		This always succeeds and returns zero.
  */
 #define __BPF_FUNC_MAPPER(FN)		\
 	FN(unspec),			\
@@ -5384,6 +5401,8 @@ union bpf_attr {
 	FN(ima_file_hash),		\
 	FN(kptr_xchg),			\
 	FN(map_lookup_percpu_elem),     \
+	FN(packet_dequeue),		\
+	FN(packet_drop),		\
 	/* */
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
