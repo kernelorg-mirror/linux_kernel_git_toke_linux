661a7297cd250763e870a021c2a33b55e2d046a9
Toke Høiland-Jørgensen <toke@redhat.com>
2024-07-26 18:17:28 +0200

    HACK: Allow tracing programs to call bpf_timer_*() functions
    Calling bpf_timer_* functions from tracepoints was disallowed in commit:
    5e0bc3082e2e ("bpf: Forbid bpf_ktime_get_coarse_ns and bpf_timer_* in tracing progs")
    
    This happened because this can lead to deadlocks if a tracing program is
    attached in the wrong place. However, we want to use this for starting a timer
    from the xdp_frame_return tracepoint to re-start packet transmission. Since this
    use of the bpf_timer_* functions cannot result in the deadlock mentioned in the
    above commit, lift the restriction for testing purposes, until we figure out a
    better way to deal with this.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -17807,13 +17807,6 @@ static int check_map_prog_compatibility(struct bpf_verifier_env *env,
 		}
 	}
 
-	if (btf_record_has_field(map->record, BPF_TIMER)) {
-		if (is_tracing_prog_type(prog_type)) {
-			verbose(env, "tracing progs cannot use bpf_timer yet\n");
-			return -EINVAL;
-		}
-	}
-
 	if ((bpf_prog_is_offloaded(prog->aux) || bpf_map_is_offloaded(map)) &&
 	    !bpf_offload_prog_map_match(prog, map)) {
 		verbose(env, "offload device mismatch between prog and map\n");
